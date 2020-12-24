94b695744838eaf268ada204ab5b756f982e5d94
Lawrence <lawrence1992.pr@gmail.com>
2020-12-24 20:38:42 +0530

    Updates

---
+++ precache-manifest.cf61305f86acbf1bfc61f98c1c62e4d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab5641e55a6d1e1fdd3e",
+    "url": "/IMDB-search/static/css/main.0c6aa0a2.chunk.css"
+  },
+  {
+    "revision": "ab5641e55a6d1e1fdd3e",
+    "url": "/IMDB-search/static/js/main.ab5641e5.chunk.js"
+  },
+  {
+    "revision": "63685b0b5d6e97e2a9aa",
+    "url": "/IMDB-search/static/js/1.63685b0b.chunk.js"
+  },
+  {
+    "revision": "505794b2ba76b733c6f3",
+    "url": "/IMDB-search/static/js/runtime~main.505794b2.js"
+  },
+  {
+    "revision": "01f2fc23e360603f46c606d91c00c7d9",
+    "url": "/IMDB-search/index.html"
+  }
+];
